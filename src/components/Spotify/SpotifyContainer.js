3ff7c7f055823091d6b9163ce8a8aee79ae2b8d7
Alex Vieyra <vieyrafc@gmail.com>
2021-03-22 13:19:42 -0600

    Scroll en listas spotify

--- src/components/Spotify/SpotifyContainer.js
+++ src/components/Spotify/SpotifyContainer.js
@@ -2,6 +2,7 @@ import { faChevronLeft, faChevronRight, faSpinner } from '@fortawesome/free-soli
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './_spotify-container.scss';
 import SpotifyItem from './SpotifyItem';
+import { useRef } from 'react';
 
 export default function SpotifyContainer({ title, data, imagesKey = 'images' }) {
   let container;
@@ -15,17 +16,26 @@ export default function SpotifyContainer({ title, data, imagesKey = 'images' }) 
     container = data.map(({ name, [imagesKey]: images }) => <SpotifyItem key={name} images={images} name={name} />);
   }
 
+  const containerRef = useRef();
+
+  function handleScroll(isBackwards = false) {
+    const amount = isBackwards ? -containerRef.current.offsetWidth : containerRef.current.offsetWidth;
+    containerRef.current.scrollLeft = containerRef.current.scrollLeft + amount;
+  }
+
   return (
     <div className="spotify-container">
       <div className="spotify-container__header">
         <p>{title}</p>
         <span />
         <div>
-          <FontAwesomeIcon icon={faChevronLeft} />
-          <FontAwesomeIcon icon={faChevronRight} />
+          <FontAwesomeIcon onClick={() => handleScroll(true)} icon={faChevronLeft} />
+          <FontAwesomeIcon onClick={() => handleScroll()} icon={faChevronRight} />
         </div>
       </div>
-      <div className="spotify-container__row">{container}</div>
+      <div className="spotify-container__row" ref={containerRef}>
+        {container}
+      </div>
     </div>
   );
 }
